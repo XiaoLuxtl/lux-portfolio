d996eac1e55248d9e0e53c293d6b1bf8fa0ea225
XiaoLux <dcarlos.mx@gmail.com>
2023-06-19 17:31:15 -0600

    Refactor using modal portal

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,64 +1,83 @@
 /* eslint-disable react/prop-types */
-import { useState, useContext } from "react";
+import { useContext } from "react";
 
-import { Modal } from "./Modal";
-import { Modal2 } from "./Modal2";
 import { GithubSVG } from "./svg/GithubSVG";
 import { ModalContext } from "../Context/ModalContext";
 
-export function Card({ title, img, p, linkText, link }) {
-  const [isModalOpen, setModalOpen] = useState(false);
+export function Card({ title, img, p, linkText, link, linkGithub }) {
   const { handleModal } = useContext(ModalContext);
-
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
 
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl glass">
-      <figure className="w-5/12">
+      <figure className="w-full md:w-5/12">
         <img
           className="h-full"
           src={img}
           alt={`Github project preview image for the project ${title}`}
         />
       </figure>
-      <div className="card-body w-7/12">
+      <div className="card-body w-full md:w-7/12">
         <h2 className="card-title font-bold">{title}</h2>
-        <p>{p}</p>
+        <p className="py-2">{p}</p>
         <div className="card-actions justify-end">
           {/* <Modal title={title} p={p} linkText={linkText} link={link} /> */}
           <div className="flex flex-row gap-2">
-            <button className="btn btn-primary hover:text-accent-focus w-16">
-              <GithubSVG />
-            </button>
-            <button
-              className="btn btn-primary hover:text-accent-focus"
-              onClick={handleOpenModal}
-            >
-              {linkText}
-            </button>
-            <Modal2
-              className="fixed top-0 left-0"
-              isOpen={isModalOpen}
-              onClose={handleCloseModal}
-            >
-              <h3 className="font-bold text-lg">{title}</h3>
-              <p>{p}</p>
-              <iframe
-                className="w-full h-full"
-                src={link}
-                frameBorder="0"
-                scrolling="yes"
-              ></iframe>
-            </Modal2>
+            {linkGithub && (
+              <form action={linkGithub} method="get" target="_blank">
+                <button className="btn btn-primary hover:text-accent-focus w-16">
+                  <GithubSVG />
+                </button>
+              </form>
+            )}
+
+            {/* Not github link and website link open the website */}
+            {!linkGithub && link ? (
+              <form action={link} method="get" target="_blank">
+                <button className="btn btn-primary hover:text-accent-focus w-16">
+                  {linkText}
+                </button>
+              </form>
+            ) : (
+              <button
+                className="btn btn-primary hover:text-accent-focus"
+                onClick={() =>
+                  handleModal(
+                    <ContentComponent p={p} title={title} link={link} />
+                  )
+                }
+              >
+                {linkText}
+              </button>
+            )}
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+function ContentComponent({ p, title, link }) {
+  return (
+    <div>
+      <h3 className="font-bold text-lg">{title}</h3>
+      <p className="py-4 text-xs">
+        Press ESC key or click on ✕ button to close.{" "}
+        <a
+          className="link-primary"
+          href={link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open in new window.
+        </a>
+      </p>
+      <p>{p}</p>
+      <iframe
+        className="w-full h-full min-h-[70vh]"
+        src={link}
+        frameBorder="0"
+        scrolling="no"
+      ></iframe>
     </div>
   );
 }
