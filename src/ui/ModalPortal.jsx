17cd23f4e8c485883db8e0256063fe9a6d0705b3
XiaoLux <dcarlos.mx@gmail.com>
2023-06-19 17:30:39 -0600

    Modal refactor

--- src/ui/ModalPortal.jsx
+++ src/ui/ModalPortal.jsx
@@ -10,14 +10,14 @@ const ModalPortal = () => {
         className="fixed top-0 left-0 h-screen w-full flex items-center justify-center"
         style={{ background: "rgba(0,0,0,0.8)" }}
       >
-        <div className="bg-white relative p-5 shadow-lg rounded flex flex-col items-start text-lg text-gray-800">
+        <div className="modal-box bg-base-100 relative h-[95vh] min-w-[80vw]">
           <button
-            className="absolute top-0 right-0 -mt-12 font-bold self-end rounded-full mb-3 bg-white text-red-700 w-8 h-8"
+            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
             onClick={() => handleModal()}
           >
-            &times;
+            ✕
           </button>
-          <p>{modalContent}</p>
+          <div>{modalContent}</div>
         </div>
       </div>,
       document.querySelector("#modal-root")
