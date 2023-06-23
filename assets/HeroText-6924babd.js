4a2bf5aa0a72052e416fee1dd7a0f55b30021067
XiaoLux <dcarlos.mx@gmail.com>
2023-06-22 22:24:29 -0600

    Updates

---
+++ assets/HeroText-6924babd.js
@@ -0,0 +1,7 @@
+import{j as r}from"./index-c2861d17.js";function t(){return r.jsxs(r.Fragment,{children:["Transforming"," ",r.jsx("span",{className:`text-transparent\r
+        bg-clip-text bg-gradient-to-r\r
+        from-primary\r
+        via-amber-500\r
+        to-secondary\r
+        background-animate\r
+        `,children:"ideas"})," ","into beautiful websites"]})}export{t as default};
